2759296c3e708ba9cc07ce593344092c39b112c4
Andreeva <andikatya@gmail.com>
2020-09-18 21:24:32 +0300

    anketa

--- source/js/tasks.js
+++ source/js/tasks.js
@@ -2,7 +2,7 @@
   
 var btnAnketa = document.querySelector('.taskAnketa');
 
-function getString (question) {
+function getName (question) {
   var str = "";
   while (!isNaN(Number(str))) {
     str = prompt(question);
@@ -10,21 +10,30 @@ function getString (question) {
   return str;
 }
 
-function getNumber (question) {
-  var nbr = 0;
-  while (isNaN(Number(nbr))) {
-    nbr = prompt(question);
+function getAge (question) {
+  var age = 0;
+  while (!age || age<=0 || age>125) {
+    age = Number(prompt(question));
   };
-  return nbr;
+  return parseInt(age);
+}
+
+function isOnRest (age, sex) {
+  var ageForRest = (sex==="мужской") ? 65 : 60;
+  return (age>=ageForRest ? "да" : "нет");
 }
   
 btnAnketa.addEventListener('click', (event) => {
-  //var lastName = getString("Введите вашу фамилию");
-  //var name = getString("Введите ваше имя");
-  //var patrName = getString("Введите ваше отчество");
-  var age = getNumber("Введите ваш возраст");
-  //var sex = confirm("Выберите ваш пол: \n - Нажмите \"Ок\", если вы мужчина \n - Нажмите \"Отмена\", если вы женщина!") ? "мужчина" : "женщина";
-    
-  //console.log(lastName + " " + name + " " + patrName + sex );
-  console.log(sex );
+  var lastName = getName("Введите вашу фамилию");
+  var firstName = getName("Введите ваше имя");
+  var patrName = getName("Введите ваше отчество");
+  var age = getAge("Введите ваш возраст");
+  var sex = confirm("Выберите ваш пол: \n - Нажмите \"Ок\", если вы мужчина \n - Нажмите \"Отмена\", если вы женщина!") ? "мужчина" : "женщина";
+  var result = "ваше ФИО: " + lastName + " " + firstName + " " + patrName + "\n"
+             + "ваш возраст в годах: " + age + "\n"
+             + "ваш возраст в днях: " + (age*365) + "\n"
+             + "через 5 лет вам будет: " + (age+5) + "\n"
+             + "ваш пол: " + sex + "\n"
+             + "вы на пенсии: " + isOnRest(age, sex);
+  alert(result);
 });
